221411b26031c739d16f80502b1754eb998bf484
Mile Balenovic <mile.balnovic@shape404.agency>
2022-08-16 20:53:50 +0200

    fix global queryClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import GlobalStyles from "../styles/GlobalStyles";
-
-const queryClient = new QueryClient();
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
